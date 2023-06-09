17ea8d410597d10ece963ca7ddce57c628c90b57
Екатерина Асташкина <32179225+astashkina-ea@users.noreply.github.com>
2023-06-09 23:17:54 +0300

    Update SimpleTest.java

--- src/test/java/lesson/SimpleTest.java
+++ src/test/java/lesson/SimpleTest.java
@@ -19,4 +19,23 @@ public class SimpleTest {
     void simpleTest() {
         Assertions.assertEquals(3, 2 + 1, "Результат дб 3");
     }
+    
+    // assertEquals() и assertNotEquals() помогают убедиться в том, что фактическое значение идентично с ожидаемым и наоборот.
+
+    // assertTrue()
+    // Если утверждение в скобках верно, то тест пройдет.
+
+    // assertFalse()
+    // Если утверждение в скобка ложно, то тест пройдет.
+
+//    assertAll()
+//    Позволяет проверить сразу несколько утверждений. Даже если одно из них упадет, то все равно будут выполнены все до самого конца, а система покажет те, которые упали.
+//
+//    Пример:
+//
+//            Assertions.assertAll(
+//            () -> Assertions.assertFalse(10 < 2),
+//            () -> Assertions.assertTrue(5 > 2),
+//            () -> Assertions.assertEquals("Good", "Good")
+//            );
 }
